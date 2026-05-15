d0475841285486fa8570203159714ce78692ea46
systemg77777-collab <systemg77777@gmail.com>
2026-05-15 13:17:34 +0800

    Update AuthService.java

--- src/main/java/com/university/ojt/service/AuthService.java
+++ src/main/java/com/university/ojt/service/AuthService.java
@@ -4,6 +4,7 @@ import com.university.ojt.model.*;
 import com.university.ojt.repository.*;
 import com.university.ojt.security.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
