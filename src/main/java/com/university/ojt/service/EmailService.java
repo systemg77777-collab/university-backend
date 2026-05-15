e66b460691ebdf0f94bd17e2bf531e7e1d56020c
systemg77777-collab <systemg77777@gmail.com>
2026-05-15 13:00:50 +0800

    Update EmailService.java

--- src/main/java/com/university/ojt/service/EmailService.java
+++ src/main/java/com/university/ojt/service/EmailService.java
@@ -1,3 +1,5 @@
+package com.university.ojt.service;
+
 import com.resend.*;
 import com.resend.services.emails.model.CreateEmailOptions;
 import com.resend.services.emails.model.CreateEmailResponse;
@@ -7,30 +9,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-    @Autowired 
-    private EmailService emailService;
-    
     @Value("${app.resend.api-key}")
     private String apiKey;
 
     @Value("${app.mail.from}")
     private String fromEmail;
 
-    public void sendResetCode(String toEmail, String code) {
+    public void sendPasswordResetEmail(String toEmail, String resetLink) {
         Resend resend = new Resend(apiKey);
 
         CreateEmailOptions params = CreateEmailOptions.builder()
             .from(fromEmail)
             .to(toEmail)
-            .subject("Your OJT System Reset Code")
-            .html("<strong>Your password reset code is: " + code + "</strong>")
+            .subject("Password Reset Request - OJT System")
+            .html("<p>You requested a password reset. Click the link below to reset your password:</p>" +
+                  "<a href='" + resetLink + "'>Reset Password</a><br><br>" +
+                  "<p>If the button doesn't work, copy and paste this link into your browser:</p>" +
+                  "<p>" + resetLink + "</p>")
             .build();
 
         try {
             CreateEmailResponse data = resend.emails().send(params);
-            System.out.println("Email sent successfully! ID: " + data.getId());
+            System.out.println("Resend API Success! ID: " + data.getId());
         } catch (Exception e) {
-            System.err.println("Failed to send email: " + e.getMessage());
+            System.err.println("Resend API Error: " + e.getMessage());
         }
     }
 }
